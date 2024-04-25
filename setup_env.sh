d8f9bd4835ee6097736f6f7721d3e77b227e607a
kathir <kathirksw@gmail.com>
2024-04-25 10:31:50 +0530

    removed setu-translate

--- setup_env.sh
+++ setup_env.sh
@@ -6,10 +6,16 @@ pip install torch torchvision torchaudio
 pip install datasets transformers
 pip install flax
 
-git clone https://github.com/kathir-ks/setu-translate
+# git clone https://github.com/kathir-ks/setu-translate
 
-cd setu-translate/
-cd IndicTransTokenizer/
+# cd setu-translate/
+# cd IndicTransTokenizer/
+# pip install --editable ./
+
+# Clone and install IndicTransTokenizer
+git clone https://github.com/VarunGumma/IndicTransTokenizer
+cd IndicTransTokenizer
 pip install --editable ./
+cd ..
 
 sudo shutdown -r now #``Restart the VM to reflect the changes in the environment.
