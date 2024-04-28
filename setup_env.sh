eb5b7d3e2464832cb3d03fd864b75d6e71f44af2
kathir <kathirksw@gmail.com>
2024-04-28 17:46:46 +0530

    Add nltk in env

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,7 @@
 pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install torch 
 pip install datasets transformers
-pip install flax
+pip install flax nltk
 
 # git clone https://github.com/kathir-ks/setu-translate
 mkdir flax_weights
